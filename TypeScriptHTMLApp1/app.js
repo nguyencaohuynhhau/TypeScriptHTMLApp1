4917f9eed5652a25be3ead647cb551be042efb4d
hau.nguyen <wwwillrockyou06@gmail.com>
2021-03-05 15:20:54 +0700

    + enum

--- TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/app.js
@@ -14,5 +14,24 @@ var nhan_vat_game = (function () {
     return nhan_vat_game;
 }());
 var nhanVat1 = new nhan_vat_game('Ire', 'Y chi luoi kiem', 697.2);
-console.log(nhanVat1.hienthiten());
+//console.log(nhanVat1.hienthiten())
+var state;
+(function (state) {
+    state[state["Create"] = 10] = "Create";
+    state[state["Processing"] = 11] = "Processing";
+    state[state["finish"] = 12] = "finish";
+})(state || (state = {}));
+var congViec = (function () {
+    function congViec(id, ten, trang_thai) {
+        this.id = id;
+        this.ten = ten;
+        this.trang_thai = trang_thai;
+    }
+    congViec.prototype.thongTin = function () {
+        return this.id + " - ten: " + this.ten + "  - trangthai: " + this.trang_thai + "   ";
+    };
+    return congViec;
+}());
+var cv1 = new congViec(1, 'hoc html', state.Create);
+console.log(cv1.thongTin());
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp1/app.ts
+++ TypeScriptHTMLApp1/app.ts
@@ -16,4 +16,23 @@
     }
 }
 var nhanVat1 = new nhan_vat_game('Ire', 'Y chi luoi kiem', 697.2);
-console.log(nhanVat1.hienthiten())
+//console.log(nhanVat1.hienthiten())
+
+enum state  { Create = 10, Processing, finish }
+class congViec {
+    id: number;
+    ten: string;
+    trang_thai: state;
+
+    constructor(id: number, ten: string, trang_thai: state) {
+        this.id = id;
+        this.ten = ten;
+        this.trang_thai = trang_thai;
+    }
+    thongTin() {
+        return `${this.id} - ten: ${this.ten}  - trangthai: ${this.trang_thai}   `;
+    }
+}
+
+let cv1 = new congViec(1, 'hoc html', state.Create)
+console.log(cv1.thongTin())
