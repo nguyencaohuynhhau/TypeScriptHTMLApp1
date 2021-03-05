f0397bea5be0c75d5240d3c1d089417b669d9ecb
hau.nguyen <wwwillrockyou06@gmail.com>
2021-03-05 15:09:04 +0700

    ...

--- TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/app.js
@@ -1,4 +1,18 @@
-var chu;
-chu = 100;
-console.log(chu + "");
+var nhan_vat_game = (function () {
+    // de tao ra instance(nhan vat thuc)
+    function nhan_vat_game(ten_nhan_vat, slogan, mau) {
+        //this la instance vua tao
+        this.ten_nhan_vat = ten_nhan_vat;
+        this.slogan = slogan;
+        this.mau = mau;
+    }
+    nhan_vat_game.prototype.chay = function () { };
+    nhan_vat_game.prototype.chem = function () { };
+    nhan_vat_game.prototype.hienthiten = function () {
+        return "nhan vat: " + this.ten_nhan_vat + ", slogan: " + this.slogan + ", mau: " + this.mau + ",";
+    };
+    return nhan_vat_game;
+}());
+var nhanVat1 = new nhan_vat_game('Ire', 'Y chi luoi kiem', 697.2);
+console.log(nhanVat1.hienthiten());
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp1/app.ts
+++ TypeScriptHTMLApp1/app.ts
@@ -1,6 +1,19 @@
-﻿var chu: any; 
-chu = 100;
-console.log((chu as string) + "");
-
-
-  
+﻿class nhan_vat_game {
+    ten_nhan_vat: string;
+    slogan: string;
+    mau: number;
+    // de tao ra instance(nhan vat thuc)
+    constructor(ten_nhan_vat: string, slogan: string, mau: number) {
+        //this la instance vua tao
+        this.ten_nhan_vat = ten_nhan_vat;
+        this.slogan = slogan;
+        this.mau = mau;
+    }
+    chay() { }
+    chem() { }
+    hienthiten() {
+        return `nhan vat: ${this.ten_nhan_vat}, slogan: ${this.slogan}, mau: ${this.mau},`
+    }
+}
+var nhanVat1 = new nhan_vat_game('Ire', 'Y chi luoi kiem', 697.2);
+console.log(nhanVat1.hienthiten())
